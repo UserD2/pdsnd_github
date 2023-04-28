c2e145ff53b7c6f59433797245767ef5ebdf4e90
Wiktor Ptak <wiktorptakofficial@gmail.com>
2023-04-28 09:54:34 +0200

    DOCs remove commented out unused code

--- bikeshare.py
+++ bikeshare.py
@@ -185,12 +185,10 @@ def trip_duration_stats(df):
     # display total travel time
     total_travel_time = df['Trip Duration'].sum()
     print('Total travel time in HH:MM:SS', convert_sec(total_travel_time))
-    #print('Total travel time in hours:', round(total_travel_time/3600, 2))
 
     # display mean travel time
     mean_travel_time = df['Trip Duration'].mean()
     print('Mean travel time in HH:MM:SS', convert_sec(mean_travel_time))
-    #print('Mean travel time in hours:', round(mean_travel_time/3600,2))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
